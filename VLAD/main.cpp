f9be819587b46ee1c832d90efa0bc81d2cae5b27
Niko <niko.gaedicke@web.de>
2018-01-09 18:18:35 +0100

    Modified main.cpp - Set the order of the training algorithm

--- VLAD/main.cpp
+++ VLAD/main.cpp
@@ -1,7 +1,33 @@
+#include "Training\BOWKTrainer.h"
+#include "Training\sorter.h"
+#include "Training\surf.h"
+#include "Training\svm.h"
+#include "Training\VLAD.h"
+
+#include "Test\svmTest_VLAD.h"
+
+#include <iostream>
+#include <new>
+#include <memory>
+
 int main() {
-  //sorter();
-  //surf();
-  //BOWKTrainer();
-  //vlad();
-  //svm_train();
+  std::unique_ptr<Sorter> pSorter(new Sorter());
+  std::unique_ptr<Surf> pSurf(new Surf());
+  std::unique_ptr<BOWKTrainer> pBOWKTrainer(new BOWKTrainer());
+  std::unique_ptr<VLAD> pVLAD(new VLAD());
+  std::unique_ptr<SVMTraining> pSVMTraining(new SVMTraining());
+
+  std::unique_ptr<SVMTest> pSVMTest(new SVMTest());
+
+  //-- Training
+  pSorter->sort();
+  pSurf->surf();
+  pBOWKTrainer->createBOWDescriptors();
+  pVLAD->bowKMeansRecogniser();
+  pSVMTraining->train();
+
+  //-- Test
+  pSVMTest->test();
+
+
 }
